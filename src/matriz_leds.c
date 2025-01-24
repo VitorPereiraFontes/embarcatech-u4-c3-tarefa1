f5b181ef46d2d5dc6295c9be0a885cc95a86c61e
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-01-24 13:43:53 -0300

    Adicionando arquivo matriz_leds.c
    Importando bibliotecas e definindo a porta que conecta o
    microcontrolador a matriz de LED's.

---
+++ src/matriz_leds.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "matriz_leds.h"
+
+// Arquivo .pio para controle da matriz
+#include "pio_matrix.pio.h"
+
+// Pino que realizará a comunicação do microcontrolador com a matriz
+#define OUT_PIN 7
