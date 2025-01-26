c90f68a82630673605c3154447b5add1b6451cfb
yohannd1 <mitonanan12@gmail.com>
2025-01-25 22:05:28 -0300

    refazendo sistema de animações

--- src/animacoes/animacoes.h
+++ src/animacoes/animacoes.h
@@ -1,6 +1,15 @@
 #ifndef _ANIMACOES_H
 #define _ANIMACOES_H
 
-void iniciar_animacao_fogos_artificio(PIO pio, uint sm);
+#include "../matriz_leds.h"
+
+typedef struct {
+    const Matriz_leds_config *frames;
+    size_t tamanho;
+    uint32_t duracao_frame_ms;
+} Animacao;
+
+Animacao obter_anim_padrao();
+Animacao obter_anim_fogos_artificio();
 
 #endif
