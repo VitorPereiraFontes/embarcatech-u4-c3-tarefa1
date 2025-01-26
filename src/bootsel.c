79cdb02ebe97fefc6aa5a60b16074ea54901d21c
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-01-26 11:56:27 -0300

    Atualizando o arquivo bootsel.c
    Atualizando a importação da biblioteca.
    
    Atualizando a função que dispara o reboot.

--- src/bootsel.c
+++ src/bootsel.c
@@ -1,10 +1,10 @@
-#include "pico/reboot.h"  // Biblioteca necessária para reiniciar o sistema
+#include "pico/bootrom.h"  // Biblioteca necessária para reiniciar o sistema
 
 // Função para reiniciar o Raspberry Pi Pico W e entrar no modo de gravação (bootloader)
 void check_reboot(char key) {
     if (key == '*') {  // Se a tecla '*' for pressionada
         printf("Tecla '*' pressionada! Reiniciando...\n");
         sleep_ms(1000);  // Atraso para exibir a mensagem antes de reiniciar
-        reboot();  // Reinicia o Raspberry Pi Pico W e entra no modo de gravação
+        reset_usb_boot(0,0);  // Reinicia o Raspberry Pi Pico W e entra no modo de gravação
     }
 }
