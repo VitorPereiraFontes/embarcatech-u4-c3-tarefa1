d5b1610c1b9ea783313786fd4fcf42d88e0e9c1a
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-01-26 15:27:35 -0300

    Adicionando a animação dos leds na cor vermelha

---
+++ src/animacoes/red.c
@@ -0,0 +1,28 @@
+#include <string.h>
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+
+#include "animacoes.h"
+
+static Matriz_leds_config animacao[1];
+
+Animacao obter_anim_red() {
+    RGB_cod blue = cor_24bit_para_3double(255,0,0);
+
+    Matriz_leds_config frame1 = {
+        {blue,blue,blue,blue,blue},
+        {blue,blue,blue,blue,blue},
+        {blue,blue,blue,blue,blue},
+        {blue,blue,blue,blue,blue},
+        {blue,blue,blue,blue,blue},
+    };
+
+    memcpy(animacao[0], frame1, sizeof(frame1));
+
+    return (Animacao) {
+        .frames = animacao,
+        .tamanho = 1,
+        .duracao_frame_ms = 100,
+    };
+}
