68ac832567f2aec362a3cb704c5be172c9abaf66
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-01-26 15:28:50 -0300

    Adicionando a animação dos leds apagados

---
+++ src/animacoes/off.c
@@ -0,0 +1,28 @@
+#include <string.h>
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+
+#include "animacoes.h"
+
+static Matriz_leds_config animacao[1];
+
+Animacao obter_anim_off() {
+    RGB_cod off = {0,0,0};
+
+    Matriz_leds_config frame1 = {
+        {off,off,off,off,off},
+        {off,off,off,off,off},
+        {off,off,off,off,off},
+        {off,off,off,off,off},
+        {off,off,off,off,off},
+    };
+
+    memcpy(animacao[0], frame1, sizeof(frame1));
+
+    return (Animacao) {
+        .frames = animacao,
+        .tamanho = 1,
+        .duracao_frame_ms = 100,
+    };
+}
